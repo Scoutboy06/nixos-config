0b4e63bf98e6d7c0f79653610f47a56cd43ddd04
Elias Wennerlund <elias06wennerlund@gmail.com>
2025-04-02 15:13:14 +0000

    some changes

--- flake.nix
+++ flake.nix
@@ -1,38 +1,81 @@
 {
-  description = "WSL NixOS configuration";
+  description = "NixOS configuration";
 
-  inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  inputs.nixpkgs.url = "github:nixos/nixpkgs/nixos-24.11";
+  inputs.nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable";
 
-    home-manager = {
-      url = "github:nix-community/home-manager";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
+  inputs.home-manager.url = "github:nix-community/home-manager/release-24.11";
+  inputs.home-manager.inputs.nixpkgs.follows = "nixpkgs";
 
-    nixos-wsl = {
-      url = "github:nix-community/NixOS-WSL/main";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
-  };
+  inputs.nur.url = "github:nix-community/NUR";
 
-  outputs = { self, nixpkgs, nixos-wsl, home-manager, ... }:
+  inputs.nixos-wsl.url = "github:nix-community/NixOS-WSL";
+  inputs.nixos-wsl.inputs.nixpkgs.follows = "nixpkgs";
+
+  inputs.nix-index-database.url = "github:Mic92/nix-index-database";
+  inputs.nix-index-database.inputs.nixpkgs.follows = "nixpkgs";
+
+  # inputs.jeezyvim.url = "github:LGUG2Z/JeezyVim";
+
+  outputs = inputs:
+    with inputs;
     let
-      system = "x86_64-linux";
-      pkgs = import nixpkgs { inherit system; };
-    in {
-      nixosConfigurations.elias = nixpkgs.lib.nixosSystem {
-        system = system;
-        modules = [
-          ./configuration.nix
-          nixos-wsl.nixosModules.default
-          home-manager.nixosModules.default
-        ];
+      # secrets = builtins.fromJSON (builtins.readFile "${self}/secrets.json");
+
+      nixpkgsWithOverlays = system:
+        (import nixpkgs rec {
+          inherit system;
+
+          config = {
+            allowUnfree = true;
+            permittedInsecurePackages = [
+              # FIXME:: add any insecure packages you absolutely need here
+            ];
+          };
+
+          overlays = [
+            nur.overlays.default
+            # jeezyvim.overlays.default
+
+            (_final: prev: {
+              unstable = import nixpkgs-unstable {
+                inherit (prev) system;
+                inherit config;
+              };
+            })
+          ];
+        });
+
+      configurationDefaults = args: {
+        home-manager.useGlobalPkgs = true;
+        home-manager.useUserPackages = true;
+        home-manager.backupFileExtension = "hm-backup";
+        home-manager.extraSpecialArgs = args;
       };
 
-      homeConfigurations.elias = home-manager.lib.homeManagerConfiguration {
-        pkgs = pkgs;
+      argDefaults = {
+        inherit inputs self nix-index-database; # secrets;
+        channels = { inherit nixpkgs nixpkgs-unstable; };
+      };
 
-        modules = [ ./home.nix ];
+      mkNixosConfiguration =
+        { system ? "x86_64-linux", hostname, username, args ? { }, modules, }:
+        let specialArgs = argDefaults // { inherit hostname username; } // args;
+        in nixpkgs.lib.nixosSystem {
+          inherit system specialArgs;
+          pkgs = nixpkgsWithOverlays system;
+          modules = [
+            (configurationDefaults specialArgs)
+            home-manager.nixosModules.home-manager
+          ] ++ modules;
+        };
+    in {
+      formatter.x86_64-linux = nixpkgs.legacyPackages.x86_64-linux.alejandra;
+
+      nixosConfigurations.nixos = mkNixosConfiguration {
+        hostname = "nixos";
+        username = "elias";
+        modules = [ nixos-wsl.nixosModules.wsl ./wsl.nix ];
       };
     };
 }
--- home.nix
+++ home.nix
@@ -1,60 +1,258 @@
-{ config, pkgs, ... }:
+{ pkgs, username, nix-index-database,
+# FIXME: uncomment the next line if you want to reference your GitHub/GitLab access tokens and other secrets
+# secrets,
+... }:
+let
+  unstable-packages = with pkgs.unstable; [
+    # FIXME: select your core binaries that you walways want on the bleeding-edge
+    # bar
+    # bottom
+    coreutils
+    curl
+    # du-dust
+    # fd
+    # findutils
+    # fx
+    git
+    # git-crypt
+    htop
+    tree
+    # jq
+    # killall
+    # mosh
+    # procs
+    # ripgrep
+    # sd
+    tmux
+    tree
+    unzip
+    vim
+    wget
+    zip
+  ];
 
-{
-  imports = [ ./modules/tmux.nix ];
+  stable-packages = with pkgs; [
+    # FIXME: customize these stable packages to your liking for the languages you use
 
-  home.username = "elias";
-  home.homeDirectory = "/home/elias";
+    # key tools
+    gh # for bootstrapping
+    just
+
+    # core languages
+    rustup
+
+    # rust stuff
+    cargo-cache
+    cargo-expand
+
+    # local dev stuff
+
+    # treesitter
+    tree-sitter
+
+    # language servers
+    nodePackages.vscode-langservers-extracted # html, css, json, eslint
+    nodePackages.yaml-language-server
+    nil # nix
+
+    # formatters and linters
+    alejandra # nix formatter
+    deadnix # nix dead code finder
+    nodePackages.prettier # js formatter
+    shellcheck # bash/sh static analyzer
+    shfmt # shell formatter
+    statix # nix linting and suggestions
+  ];
+in {
+  imports = [
+    nix-index-database.hmModules.nix-index
+    # ./modules/tmux.nix
+  ];
+
   home.stateVersion = "24.11"; # Please read the documentation before changing.
 
-  programs.starship = { enable = true; };
+  home = {
+    username = "${username}";
+    homeDirectory = "/home/${username}";
 
-  programs.zsh = {
-    enable = true;
-    enableCompletion = true;
-    # autosuggestions.enable = true;
-    syntaxHighlighting.enable = true;
-    shellAliases = {
-      la = "ls -a";
-      ll = "ls -al";
-      cl = "clear";
-      update =
-        "sudo nixos-rebuild switch --flake ~/nixos-config#elias --impure";
-    };
-    history.size = 10000;
+    sessionVariables.EDITOR = "hx";
+    # FIXME: set your preferred $SHELL
+    sessionVariables.SHELL = "/etc/profiles/per-user/${username}/bin/fish";
   };
 
-  programs.helix = {
-    enable = true;
-    settings = {
-      theme = "autumn_night_transparent";
-      editor.cursor-shape = {
-        normal = "block";
-        insert = "bar";
-        select = "underline";
+  home.packages = stable-packages ++ unstable-packages ++
+    # FIXME: you can add anything else that doesn't fit into the above two lists in here
+    [
+      # pkgs.some-package
+      # pkgs.unstable.some-other-package
+    ];
+
+  programs = {
+    home-manager.enable = true;
+    nix-index.enable = true;
+    nix-index.enableFishIntegration = true;
+    nix-index-database.comma.enable = true;
+
+    # FIXME: disable this if you don't want to use the starship prompt
+    starship.enable = true;
+    starship.settings = {
+      aws.disabled = true;
+      gcloud.disabled = true;
+      kubernetes.disabled = true;
+      git_branch_style = "242";
+      directory.style = "blue";
+      directory.truncate_to_repo = false;
+      directory.truncation_length = 8;
+      # python.disabled = true;
+      # ruby.disabled = true;
+      hostname.ssh_only = false;
+      hostname.style = "bold green";
+    };
+
+    # FIXME: disable whatever you don't want
+    fzf.enable = true;
+    fzf.enableFishIntegration = true;
+    lsd.enable = true;
+    lsd.enableAliases = true;
+    zoxide.enable = true;
+    zoxide.enableFishIntegration = true;
+    zoxide.options = [ "--cmd cd" ];
+    broot.enable = true;
+    broot.enableFishIntegration = true;
+    direnv.enable = true;
+    direnv.nix-direnv.enable = true;
+
+    helix = {
+      enable = true;
+      settings = {
+        theme = "autumn_night_transparent";
+        editor.cursor-shape = {
+          normal = "block";
+          insert = "bar";
+          select = "underline";
+        };
+      };
+      languages.language = [{
+        name = "nix";
+        auto-format = true;
+        formatter.command = "${pkgs.nixfmt}/bin/nixfmt";
+      }];
+      themes = {
+        autumn_night_transparent = {
+          "inherits" = "autumt_night";
+          "ui.background" = { };
+        };
       };
     };
-    languages.language = [{
-      name = "nix";
-      auto-format = true;
-      formatter.command = "${pkgs.nixfmt}/bin/nixfmt";
-    }];
-    themes = {
-      autumn_night_transparent = {
-        "inherits" = "autumt_night";
-        "ui.background" = { };
+
+    git = {
+      enable = true;
+      package = pkgs.unstable.git;
+      delta.enable = true;
+      delta.options = {
+        line-numbers = true;
+        side-by-side = true;
+        navigate = true;
+      };
+      userEmail = "elias06wennerlund@gmail.com";
+      userName = "Elias Wennerlund";
+      extraConfig = {
+        # FIXME: uncomment the next lines if you wantt o be able to clone private https repos
+        # url = {
+        #   "https://oauth2:${secrets.github_token}@github.com" = {
+        #     insteadOf = "https://github.com";
+        #   };
+        #   "https://oauth2:${secrets.github_token}@github.com" = {
+        #     insteadOf = "https://gitlab.com";
+        #   };
+        # };
+        push = {
+          default = "current";
+          autoSetupRemote = true;
+        };
+        merge = { conflictstyle = "diff3"; };
+        diff = { colorMoved = "default"; };
       };
     };
+
+    # FIXME: This is my fish config - you can fiddle with it if you want
+    fish = {
+      enable = true;
+      # FIXME run 'scoop install win32yank' on Windows, then add this line with your Windows username to the bottom of interactiveShellInit
+      # fish_add_path --append /mnt/c/Users/<Your Windows Username>/scoop/apps/win32yank/0.1.1
+      interactiveShellInit = ''
+        ${pkgs.any-nix-shell}/bin/any-nix-shell fish --info-right | source
+
+        ${pkgs.lib.strings.fileContents (pkgs.fetchFromGitHub {
+          owner = "rebelot";
+          repo = "kanagawa.nvim";
+          rev = "de7fb5f5de25ab45ec6039e33c80aeecc891dd92";
+          sha256 = "sha256-f/CUR0vhMJ1sZgztmVTPvmsAgp0kjFov843Mabdzvqo=";
+        } + "/extras/kanagawa.fish")}
+
+        set -U fish_greeting
+      '';
+      functions = {
+        refresh = "source $HOME/.config/fish/config.fish";
+        take = ''mkdir -p -- "$1" && cd -- "$1"'';
+        ttake = "cd $(mktemp -d)";
+        show_path = ''
+          echo $PATH | tr ' ' '
+          ''';
+        posix-source = ''
+          for i in (cat $argv)
+            set arr (echo $i |tr = \n)
+            set -gx $arr[1] $arr[2]
+          end
+        '';
+      };
+      shellAbbrs = {
+        gc = "nix-collect-garbage --delete-old";
+      }
+      # navigation shortcuts
+        // {
+          ".." = "cd ..";
+          "..." = "cd ../../";
+          "...." = "cd ../../../";
+          "....." = "cd ../../../../";
+        }
+        # git shortcuts
+        // {
+          gapa = "git add --patch";
+          grpa = "git reset --patch";
+          gst = "git status";
+          gdh = "git diff HEAD";
+          gp = "git push";
+          gph = "git push -u origin HEAD";
+          gco = "git checkout";
+          gcob = "git checkout -b";
+          gcm = "git checkout master";
+          gcd = "git checkout develop";
+          gsp = "git stash push -m";
+          gsa = "git stash apply stash^{/";
+          gsl = "git stash list";
+        };
+      shellAliases = {
+        explorer = "/mnt/c/Windows/explorer.exe";
+
+        # To use code as the command, uncomment the line below. Be sure to replace [my-user] with your username.
+        # If your code binary is located elsewhere, adjust the path as needed.
+        # code = "/mnt/c/Users/[my-user]/AppData/Local/Programs/'Microsoft VS Code'/bin/code";
+      };
+      plugins = [
+        {
+          inherit (pkgs.fishPlugins.autopair) src;
+          name = "autopair";
+        }
+        {
+          inherit (pkgs.fishPlugins.done) src;
+          name = "done";
+        }
+        {
+          inherit (pkgs.fishPlugins.sponge) src;
+          name = "sponge";
+        }
+      ];
+    };
   };
-
-  programs.direnv = {
-    enable = true;
-    nix-direnv.enable = true;
-  };
-
-  home.packages = with pkgs; [ htop tree ];
-
-  home.sessionVariables = { EDITOR = "nvim"; };
-
-  programs.home-manager.enable = true;
 }
